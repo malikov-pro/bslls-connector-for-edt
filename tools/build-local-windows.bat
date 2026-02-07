6a83557fa1420c15ccc606e6348c30ea41a5b201
Nikita Fedkin <nikita.fedkin@booking.com>
2026-02-07 08:11:58 +0100

    Исправления по code review
    - tools/build-local-windows.bat: кавычки вокруг MAVEN_OPTS (пробелы в пути)
    - targets/default.target: http → https для Eclipse license repo
    - targets/default.target: Lombok 1.18.16 → 1.18.30 (синхронизация с Maven)

--- tools/build-local-windows.bat
+++ tools/build-local-windows.bat
@@ -1,4 +1,4 @@
 cd ..
 mvn dependency:copy@get-lombok -pl bundles/com.github.otymko.dt.bsl.lspconnector
-set MAVEN_OPTS=-Djdk.xml.maxGeneralEntitySizeLimit=0 -Djdk.xml.totalEntitySizeLimit=0 -javaagent:%cd%\bundles\com.github.otymko.dt.bsl.lspconnector\target\lombok.jar=ECJ
+set "MAVEN_OPTS=-Djdk.xml.maxGeneralEntitySizeLimit=0 -Djdk.xml.totalEntitySizeLimit=0 -javaagent:%cd%\bundles\com.github.otymko.dt.bsl.lspconnector\target\lombok.jar=ECJ"
 mvn clean verify -Dtycho.localArtifacts=ignore
